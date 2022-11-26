e121edce8c1f3d2b64ab00f2604247e23210c32a
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-11-26 13:20:47 -0500

    subarea joins

--- src/subareas/subareas.controllers.js
+++ src/subareas/subareas.controllers.js
@@ -3,7 +3,14 @@ const Subareas = require("../models/subareas.models");
 const Areas = require("../models/areas.models");
 
 const getSubareas = async () => {
-  const data = Subareas.findAll();
+  const data = Subareas.findAll({
+    attributes: {
+      exclude: ["areaId"]
+    },
+    include: {
+      model: Areas,
+    },
+  });
   return data;
 };
 
